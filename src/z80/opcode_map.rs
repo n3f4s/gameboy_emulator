b5b5287dfb311761ff38375096c6cd8247c8ad60
n3f4s <tiv.crb@gmail.com>
2020-05-19 17:31:09 +0200

    Print opcode in hex

--- src/z80/opcode_map.rs
+++ src/z80/opcode_map.rs
@@ -4,7 +4,7 @@ use z80::cpu::CPU;
 
 impl<'a> OpcodeMap<'a> {
     pub fn map(&self, opcode: Opcode, cpu: &mut CPU) {
-        println!("Executing opcode {} ({})", self.op_name[opcode], opcode);
+        println!("Executing opcode {} (0x{:x})", self.op_name[opcode], opcode);
         self.op_map[opcode](cpu)
     }
 }
