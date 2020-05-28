ba9ebd1d5def133450de500ec668c71900df0415
n3f4s <tiv.crb@gmail.com>
2020-05-28 12:19:56 +0200

    Use 16bit for the clock

--- src/z80/clock.rs
+++ src/z80/clock.rs
@@ -1,15 +1,15 @@
 #![allow(dead_code)] // FIXME remove
 #![allow(unused_variables)] // FIXME remove
 // FIXME: see what the clock members represents
-use z80::register::Reg8;
+use z80::register::Reg16;
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Default)]
 pub struct Clock {
-    pub m: Reg8,
-    pub t: Reg8,
+    pub m: Reg16,
+    pub t: Reg16,
 }
 
 impl Clock {
-    pub fn tick(cycle: u8) -> Clock {
+    pub fn tick(cycle: u16) -> Clock {
         Clock {
             m: cycle,
             t: 4 * cycle,
